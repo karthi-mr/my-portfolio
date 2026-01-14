b689fc8f83236826fcb00b8294a54684beb78f3b
Karthi-mr <mkarthim1998@gmail.com>
2026-01-14 10:39:17 +0530

    added footer with some links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,47 @@
 import type { ReactElement } from "react";
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 function Footer(): ReactElement {
   return (
-    <footer className="bg-slate-800 text-sm text-center text-gray-400 py-4 mt-auto">
-      &copy; {new Date().getFullYear()} Karthi. All rights reserved.
+    <footer
+      className="bg-slate-900 px-4 py-6 text-center border-t border-slate-700 text-gray-300 mt-auto
+      shadow-2xl shadow-white/90"
+    >
+      <div className="flex justify-center gap-4 mb-4">
+        <a
+          href="https://github.com/karthi-mr"
+          target="_blank"
+          className="bg-slate-800 p-2 rounded-full hover:bg-blue-500 transition"
+        >
+          <FaGithub size={20} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/karthi-mr"
+          target="_blank"
+          className="bg-slate-800 p-2 rounded-full hover:bg-blue-500 transition"
+        >
+          <FaLinkedin size={20} />
+        </a>
+        <a
+          href="https://wa.me/917708006145"
+          target="_blank"
+          className="bg-slate-800 p-2 rounded-full hover:bg-blue-500 transition text-green-500"
+        >
+          <FaWhatsapp size={20} />
+        </a>
+        <a
+          href="mailto:mkarthim1998@gmail.com"
+          target="_blank"
+          className="bg-slate-800 p-2 rounded-full hover:bg-blue-500 transition"
+        >
+          <MdEmail size={20} />
+        </a>
+      </div>
+
+      <p>
+        &copy; {new Date().getFullYear()} Karthi. All rights reserved.
+      </p>
     </footer>
   );
 }
