149a4b4fc2a4bd30aeb97699860fa18c60a17096
Karthi-mr <mkarthim1998@gmail.com>
2026-01-22 18:46:41 +0530

    added firebase deployment and Setup Firebase Hosting GitHub deploy

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,19 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          icons: ["lucide-react"],
+          // add more big libs here if you use them:
+          // ui: ["@radix-ui/react-dialog", "@radix-ui/react-popover"],
+          // charts: ["recharts"],
+          // editor: ["monaco-editor"],
+        },
+      },
+    },
+  },
 })
